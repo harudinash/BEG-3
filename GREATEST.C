8ecbbe307fa9a93031b2cb17d4148eb077f2a0ee
harudinash <46379892+harudinash@users.noreply.github.com>
2019-01-29 06:55:44 +0530

    Create GREATEST.C

---
+++ GREATEST.C
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+   int a[20],i,d,j,k,l,m,n;
+printf("Input :");
+scanf("%d",&n);
+   printf("output :\n")
+for(i=0;i<n;i++)
+  {
+   scanf("%d",&a[i]);
+   }
+     d=1;
+for(i=0;i<n;i++)
+{
+  for(j=i+1;j<n;j++)
+    {
+        if(a[i]>a[j])
+        {
+            m=a[i];
+            a[i]=a[j];
+            a[j]=m;
+        }
+    }
+    
+
+}
+printf("%d",a[n-1]);
+return 0;
+    
+}
