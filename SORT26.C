a3cc709d24f54aeb956e5ae3e56378a3b1ce18cf
harudinash <46379892+harudinash@users.noreply.github.com>
2019-02-10 09:31:17 +0530

    Create SORT26.C

---
+++ SORT26.C
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+   int a[20],i,d,j,k,l,m,n;
+printf("Input :");
+scanf("%d",&n);
+printf("output :\n");
+for(i=0;i<n;i++)
+  {
+   scanf("%d",&a[i]);
+   }
+     d=1;
+for(i=0;i<n;i++)
+{
+  for(j=i+1;j<n;j++)
+    {
+        if(a[i]>a[j])
+        {
+            m=a[i];
+            a[i]=a[j];
+            a[j]=m;
+        }
+    }
+    
+
+}
+for(i=0;i<n;i++){
+printf("%d ",a[i]);
+}
+return 0;
+    
+}
+    
+}l
